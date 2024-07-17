6848d90f1905ef01a147cd048c00cd6658c77b67
Lobul24 <kalefinley@gmail.com>
2024-07-16 22:50:27 -0400

    Se añadio un comentario

--- src/main/java/Logica/POO.java
+++ src/main/java/Logica/POO.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package Logica;
 
-/**
- *
- * @author Personal
- */
 public class POO {
 
     public static void main(String[] args) {
