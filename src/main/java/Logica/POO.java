52573a6f75bbc6394e0d888f34336d78c6935693
Lobul24 <kalefinley@gmail.com>
2024-07-17 00:23:27 -0400

    Usamos Get and Set lo ponemos en practica

--- src/main/java/Logica/POO.java
+++ src/main/java/Logica/POO.java
@@ -9,12 +9,33 @@ public class POO {
         Alumno alu1 = new Alumno();
         
         //Segundo constructor con atributos
-        Alumno alu2 = new Alumno(35 , "Rick" , "Grimes"); 
+        Alumno alu2 = new Alumno(5 , "Rick" , "Grimes"); 
         
         //muestras el ID,NOMBRE,APELLIDO con el uso de get
         System.out.println("La id del alumno2 es: " + alu2.getId());
         System.out.println("El nombre del alumno2 es: " + alu2.getNombre());
         System.out.println("El apellido del alumno2 es: " + alu2.getApellido());
+        
+        //Usamos "SET" para asignar valores al constructor vacio
+        alu1.setId(8);
+        alu1.setNombre("Jhon");
+        alu1.setApellido("Wick");
+        
+        //Mostramos los datos asignados anteriormente
+        System.out.println("---------------------------------");
+        System.out.println("La ID del alumno1 es: " + alu1.getId());
+        System.out.println("El nombre del alumno1 es: " + alu1.getNombre());
+        System.out.println("El apellido del alumno1 es " + alu1.getApellido());
+        
+        
+        //cambio de valor del ID con set y mostramos el cambio
+        alu2.setId(35);
+        System.out.println("---------------------------------");
+        System.out.println("La id del alumno2 es: " + alu2.getId());
+        System.out.println("El nombre del alumno2 es: " + alu2.getNombre());
+        System.out.println("El apellido del alumno2 es: " + alu2.getApellido());
+        
+        
         
     }
 }
