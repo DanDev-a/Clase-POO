8352d654aa8e9f68c0fa0790ae35de09e65421d2
Lobul24 <kalefinley@gmail.com>
2024-07-16 22:33:19 -0400

    Programacion Orientada a Objetos

---
+++ src/main/java/Logica/POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Logica;
+
+/**
+ *
+ * @author Personal
+ */
+public class POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
