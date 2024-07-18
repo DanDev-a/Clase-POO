5cb1eae71b94524125dc3e7f5a0289a7f8e1001d
Lobul24 <kalefinley@gmail.com>
2024-07-18 00:23:10 -0400

    Se completo la clase de polimorfismo

--- Encapsulamiento/src/main/java/Encapsular/Encapsulamiento.java
+++ Encapsulamiento/src/main/java/Encapsular/Encapsulamiento.java
@@ -4,6 +4,16 @@ package Encapsular;
 public class Encapsulamiento {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        //Si el constructor se pone en privado no se puede utilizar
+        Alumno alu1 = new Alumno();
+        
+        //Se puede utilizar el constructor si es publico
+        Alumno alu2 = new Alumno(15, "Fernando" , "Gutierrez");
+        
+        //mostramos los datos 
+        System.out.println("Id: " + alu2.getId());
+        System.out.println("Nombre: " + alu2.getNombre());
+        System.out.println("Apellido: " + alu2.getApellido());
     }
 }
