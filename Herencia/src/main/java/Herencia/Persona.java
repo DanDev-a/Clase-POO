b817f0473c000473022cf36cbf09b364f76f2eb7
Lobul24 <kalefinley@gmail.com>
2024-07-17 20:06:01 -0400

    Se agreego atributos y constructores a la PERSONA

--- Herencia/src/main/java/Herencia/Persona.java
+++ Herencia/src/main/java/Herencia/Persona.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Herencia;
 
-/**
- *
- * @author Personal
- */
 public class Persona {
+    
+    int id;
+    String dni;
+    String nombre;
+    String apellido;
+    String domicilio;
+    String telefono;
+
+    public Persona() {
+    }
+
+    public Persona(int id, String dni, String nombre, String apellido, String domicilio, String telefono) {
+        this.id = id;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+    }
+    
+    
     
 }
